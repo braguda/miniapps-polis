1d3af904b70b513143eecac2b18ad7763e0356e3
Stuart Spellman <brian.aguda@gmail.com>
2021-07-29 20:01:00 -0700

    Added other pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,40 @@
 $(document).ready(() => {
     console.log("ready");
 
+    //buttons
+    var toDo = $("#listApp");
+    var calc = $("#calcApp");
+    var restaurant = $("#restaurantApp");
+    var mapper = $("#mapApp");
+    var settings = $("#settingsApp");
+    
+    //button clickers
+
+    toDo.click(() => {
+        console.log("clicked to do")
+        window.location.href="toDo.html";
+    });
+
+    calc.click(() => {
+        console.log("clicked calculator");
+        window.location.href="calculator.html";
+    });
+
+    restaurant.click(()=> {
+        console.log("clicked restaurant");
+        window.location.href="restaurant.html";
+    });
+
+    mapper.click(() => {
+        console.log("clicked map")
+        window.location.href="mapper.html";
+    });
+
+    settings.click(() => {
+        console.log("clicked settings");
+        window.location.href="settings.html";
+    });
+
+
 
 });
